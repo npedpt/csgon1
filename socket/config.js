c8e8c44e563e3781186510c50707724eec796b32
tuz1k <bohodurk@gmail.com>
2017-12-20 14:59:48 -0800

    Update config.js

--- socket/config.js
+++ socket/config.js
@@ -1,21 +1,21 @@
 var config = {};
 
-config.db = 'csgo';
-config.host = 'localhost';
-config.user = 'root';
-config.password = '782d65bf0699aad3d4f003d2282869f352104528088f603e054d43';
+config.db = '';
+config.host = '';
+config.user = '';
+config.password = '';
 
-config.admin = '76561198338314767';
-config.botsteamid = {bot1:'76561198352064935'};
-config.identitysecret = '"MfR7mpULa15uNeyXakmbzdUfPS0=';
-config.sharedsecret = 'UNWFB0pWw90IXwbMqFyDQYL2npQ=';
+config.admin = '';
+config.botsteamid = {bot1:''};
+config.identitysecret = '';
+config.sharedsecret = '';
 config.polling_interval  = 5000;
 
-config.bot_username = 'csgopowerup5';
-config.bot_password = 'Ity1233211';
+config.bot_username = '';
+config.bot_password = '';
 
-config.manager_domain = 'csgon1.com';
+config.manager_domain = '';
 config.manager_lang = 'en';
-config.manager_cancelTime = 60000;
+config.manager_cancelTime = ;
 
 module.exports = config;
